546bb6cbb8fd97fd5a00c42d57b3640fda2b15a3
nikos <athanikos@gmail.com>
2020-07-26 16:51:24 +0300

    initial commit

---
+++ CryptoNotificationsService/tests/server_test.py
@@ -0,0 +1,16 @@
+import pytest
+from server import  create_app
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app()
+    testing_client = flask_app.test_client()
+    ctx = flask_app.app_context()
+    ctx.push()
+    yield testing_client  # this is where the testing happens!
+    ctx.pop()
+
+
+def test_fetch_transactions(test_client):
+    assert(1==1)
